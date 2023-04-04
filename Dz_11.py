d3e1c10b076d1678f18df0cf470ffb66dbd2df4b
Wasil P <it.dzemidok@gmail.com>
2023-04-04 11:01:20 +0200

    Remade Dz_11

--- Dz_11.py
+++ Dz_11.py
@@ -3,50 +3,54 @@
 # хранить операцию (снятие или пополнение), сумму, дату и время операции.
 # Создать экземпляр банковского аккаунта и проверить его работу.
 
+from dataclasses import dataclass
 from datetime import datetime
 
 class BankAcc:
 
     def __init__(self):
         self.balance = 0
-        self.last_operation = ()
+        self.history: list[BankAccountHistory] = []
 
 
     def replenishment_of_the_balance(self, summa_currency: int):
         self.summa_currency = summa_currency
+        time.sleep(1)
         self.balance += self.summa_currency
-        self.last_operation = (f"Replenishment of the balance {self.balance} {datetime.now()}")
+        self.history.append([(f"Replenishment of the balance {self.balance} {datetime.now()}")])
 
     def withdrawals(self, summa_currency: int):
         self.summa_currency = summa_currency
+        time.sleep(1)
         self.balance -= self.summa_currency
-        self.last_operation = (f"Withdrawal of funds from the balance {self.balance} {datetime.now()}")
-
-    def last_operation(self):
-        print(self.last_operation)
-
-class OperationsHistory:
-
-    def __init__(self):
-        self.last_operation_history = BankAcc()
+        self.history.append([(f"Withdrawal of funds from the balance {self.balance} {datetime.now()}")])
 
 
-    def show_last_tansactions(self):
-        print(last_operation_history.last_operation)
+@dataclass
+class BankAccountHistory:
+    history: list
 
-
-last_operation_history = BankAcc()
 
 account_Nick = BankAcc()
-operations_Nick = OperationsHistory()
+operations_Nick = BankAccountHistory(account_Nick.history)
 
 account_Nick.replenishment_of_the_balance(10)
 print(account_Nick.balance)
-account_Nick.replenishment_of_the_balance(50)
+account_Nick.replenishment_of_the_balance(30)
 print(account_Nick.balance)
 account_Nick.withdrawals(12)
 print(account_Nick.balance)
+print(operations_Nick.history)
 
-print(last_operation_history.last_operation)
+account_Mick = BankAcc()
+operations_Mick = BankAccountHistory(account_Mick.history)
+
+account_Mick.replenishment_of_the_balance(100)
+print(account_Mick.balance)
+account_Mick.withdrawals(110)
+print(account_Mick.balance)
+account_Mick.replenishment_of_the_balance(360)
+print(account_Mick.balance)
+print(operations_Mick.history)
 
 
