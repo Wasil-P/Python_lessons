73c372a4716d0df769fb41237e61e1df54d0e06f
Wasil P <it.dzemidok@gmail.com>
2023-04-08 14:53:49 +0200

    Remade Dz_12

--- Dz_12.py
+++ Dz_12.py
@@ -48,42 +48,43 @@
     # Бот выбрал ножницы, вы проиграли!
     # Подсказка: я не показывала, как в Pyhon получить случайное число, попробуйте найти это сами
 
-# class Otherwise(Exception):
-#     pass
-#
-# from random import randint
-# greetings = print('Приветствую вас! Вам нужно выбрать один из вариантов ответа, введя номер:\n '
-#                   '1 Камень\n 2 Ножницы\n 3 Бумага')
-# answer_user = int(input('Ваш выбор: '))
-# answer_bot = randint(1, 3)
-# print(answer_bot)
-#
-# if answer_user == 1:
-#     if answer_bot == 2:
-#         print('Бот выбрал Ножницы, вы победили!')
-#     elif answer_bot == 3:
-#         print('Бот выбрал Бумага, вы проиграли!')
-#     else:
-#         print('Бот тоже выбрал Камень, ничья!')
-# elif answer_user == 2:
-#     if answer_bot == 1:
-#         print('Бот выбрал Камень, вы проиграли!')
-#     elif answer_bot == 3:
-#         print('Бот выбрал Бумага, вы победили!')
-#     else:
-#         print('Бот тоже выбрал Ножницы, ничья!')
-# elif answer_user == 3:
-#     if answer_bot == 1:
-#         print('Бот выбрал Камень, вы победили!')
-#     elif answer_bot == 2:
-#         print('Бот выбрал Ножницы, вы проиграли!')
-#     else:
-#         print('Бот тоже выбрал Бумага, ничья!')
-# raise Otherwise("Вы ввели недопустимое число")
+class Otherwise(Exception):
+    pass
 
+from random import randint
 
-
-
+greetings = print('Приветствую вас! Вам нужно выбрать один из вариантов ответа, введя номер:\n '
+                  '1 Камень\n 2 Ножницы\n 3 Бумага')
+try:
+    answer_user = int(input('Ваш выбор: '))
+except ValueError:
+    print("Вы ввели недопустимое значение")
+else:
+    answer_bot = randint(1, 3)
+    print(answer_bot)
+    if answer_user == 1:
+        if answer_bot == 2:
+            print('Бот выбрал Ножницы, вы победили!')
+        elif answer_bot == 3:
+            print('Бот выбрал Бумага, вы проиграли!')
+        else:
+            print('Бот тоже выбрал Камень, ничья!')
+    elif answer_user == 2:
+        if answer_bot == 1:
+            print('Бот выбрал Камень, вы проиграли!')
+        elif answer_bot == 3:
+            print('Бот выбрал Бумага, вы победили!')
+        else:
+            print('Бот тоже выбрал Ножницы, ничья!')
+    elif answer_user == 3:
+        if answer_bot == 1:
+            print('Бот выбрал Камень, вы победили!')
+        elif answer_bot == 2:
+            print('Бот выбрал Ножницы, вы проиграли!')
+        else:
+            print('Бот тоже выбрал Бумага, ничья!')
+    else:
+        raise Otherwise("Вы ввели недопустимое число")
 
     # 2 (из ДЗ номер 5)
     # Написать функцию, которая принимает целое число n и выводит числа от 0 до n.
@@ -91,35 +92,38 @@
     # Если число делится на 7 и на 4 одновременно, процесс останавливается.
     # Если пользователь ввел не число, вывести ошибку, что введенные данные не являются числом.
 
-class EnteredNumberError(Exception):
-    pass
-
-
-def random_list_numbers(num: int):
-    list_ = [0]
-    index = 1
-    if type(num) != int or num < 0:
-            raise EnteredNumberError("the entered data is not a number, or is a negative number")
-    elif num % 2 == 0:
-        num = num ** 2
-        while index <= num:
-            if index % 7 == 0 and index % 4 == 0:
-                raise EnteredNumberError("forced stop")
-            else:
-                list_.append(index)
-            index += 1
-        return list_
-    else:
-        while index <= num:
-            if index % 7 == 0 and index % 4 == 0:
-                raise EnteredNumberError("forced stop")
-            else:
-                list_.append(index)
-            index += 1
-        return list_
-
-
-
-# print(random_list_numbers(-4))
-# print(random_list_numbers("sef"))
-# print(random_list_numbers(28))
+# class EnteredNumberError(Exception):
+#     pass
+#
+#
+# def random_list_numbers(num: int):
+#     list_ = [0]
+#     index = 1
+#     if type(num) != int or num < 0:
+#             raise EnteredNumberError("the entered data is not a number, or is a negative number")
+#     elif num % 2 == 0:
+#         num = num ** 2
+#         while index <= num:
+#             if index % 7 == 0 and index % 4 == 0:
+#                 raise EnteredNumberError("forced stop")
+#             else:
+#                 list_.append(index)
+#             index += 1
+#         return list_
+#     else:
+#         while index <= num:
+#             if index % 7 == 0 and index % 4 == 0:
+#                 raise EnteredNumberError("forced stop")
+#             else:
+#                 list_.append(index)
+#             index += 1
+#         return list_
+#
+#
+# try:
+#     print(random_list_numbers(4))
+#     print(random_list_numbers(-4))
+#     print(random_list_numbers("sef"))
+#     print(random_list_numbers(28))
+# except EnteredNumberError:
+#     print("invalid value entered")
