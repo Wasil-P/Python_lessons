6fcf7e3e810392ae6f76f767bd24ef5502d44180
Wasil P <it.dzemidok@gmail.com>
2023-04-27 15:54:51 +0200

    Remade class BankAccountHistory and class OperationName

--- Dz_16_Model.py
+++ Dz_16_Model.py
@@ -1,10 +1,13 @@
 
+import time
+from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from sqlalchemy import Enum as sqlalchemy_enum
+from enum import Enum
 
 class Base(DeclarativeBase):
     pass
@@ -21,13 +24,9 @@ def configure_engine():
         Session.configure(bind=engine)
 
 
-class OperationName(Base):
-    __tablename__ = "operationname"
-
-    id_operation: Mapped[int] = mapped_column(primary_key=True)
-    operation_name: Mapped[str]
-    bankaccounthistory: Mapped["BankAccountHistory"] = relationship(back_populates="operationname")
-
+class OperationName(Enum):
+    PLUS = "Replenishment"
+    MINUS = "Withdrawal"
 
 class BankAcc(Base):
     __tablename__ = "bankacc"
@@ -46,9 +45,19 @@ class BankAccountHistory(Base):
     bankacc: Mapped["BankAcc"] = relationship("BankAcc", back_populates="bankaccounthistory")
     amount: Mapped[int]
     date_time: Mapped[int]
-    id_operation = mapped_column(ForeignKey("operationname.id_operation"))
-    operationname: Mapped["OperationName"] = relationship("OperationName", back_populates="bankaccounthistory")
+    operation: Mapped[str] = mapped_column(sqlalchemy_enum(OperationName))
 
+    def replenishment_of_the_balance(self, amount: int):
+        self.amount = amount
+        time.sleep(1)
+        self.balance += self.amount
+        self.history.append(BankAccountHistory(amount, OperationName.PLUS, datetime.now()))
+
+    def withdrawals(self, amount: int):
+        self.amount = amount
+        time.sleep(1)
+        self.balance -= self.amount
+        self.history.append(BankAccountHistory(amount, OperationName.MINUS, datetime.now()))
 
 configure_engine()
 Base.metadata.create_all(engine)
@@ -59,10 +68,3 @@ account_Nick = BankAcc(acc_name="Nick", balance = 20)
 session.add(account_Nick)
 session.commit()
 
-operationname_plus = OperationName(operation_name="PLUS")
-session.add(operationname_plus)
-session.commit()
-
-operationname_minus = OperationName(operation_name="MINUS")
-session.add(operationname_minus)
-session.commit()
